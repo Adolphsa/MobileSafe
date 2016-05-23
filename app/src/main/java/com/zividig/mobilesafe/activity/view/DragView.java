a94f8b46eb1217710153df4c26736da0c26f4c39
Adolphsa <497749990@qq.com>
2016-05-23 17:19:06 +0800

    2016/05/23 电话归属地查询框风格可切换

---
+++ app/src/main/java/com/zividig/mobilesafe/activity/view/DragView.java
@@ -0,0 +1,87 @@
+package com.zividig.mobilesafe.activity.view;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.zividig.mobilesafe.R;
+
+/**
+ * 归属地显示控件的拖动
+ * Created by Administrator on 2016-05-23.
+ */
+public class DragView extends Activity{
+
+    private TextView tvTop;
+    private ImageView ivDrag;
+    private TextView tvBottom;
+    private int startX;
+    private int startY;
+    private SharedPreferences mPref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drag);
+
+        mPref = getSharedPreferences("config",MODE_PRIVATE);
+
+        tvTop = (TextView) findViewById(R.id.tv_top);
+        ivDrag = (ImageView) findViewById(R.id.iv_drag);
+        tvBottom = (TextView) findViewById(R.id.tv_bottom);
+
+        int lastX = mPref.getInt("lastX", 0);
+        int lastY = mPref.getInt("lastY", 0);
+        //获取布局对象
+        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) ivDrag.getLayoutParams();
+        layoutParams.leftMargin = lastX;
+        layoutParams.topMargin = lastY;
+        ivDrag.setLayoutParams(layoutParams);
+
+        ivDrag.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_DOWN: //按下
+                        //获取起点的X,Y坐标
+                        startX = (int) event.getRawX();
+                        startY = (int) event.getRawY();
+                        break;
+                    case MotionEvent.ACTION_MOVE: //移动
+                        //获取终点的X,Y坐标
+                        int endX = (int) event.getRawX();
+                        int endY = (int) event.getRawY();
+
+                        //计算x,y的偏移量
+                        int dx = endX - startX;
+                        int dy = endY - startY;
+
+                        //绘制控件位置
+                        int l = ivDrag.getLeft() + dx; //左
+                        int t = ivDrag.getTop() + dy; //上
+                        int r = ivDrag.getRight() + dx; //右
+                        int b = ivDrag.getBottom() + dy; //下
+                        ivDrag.layout(l,t,r,b);
+
+                        //重新初始化起点坐标
+                        startX = (int) event.getRawX();
+                        startY = (int) event.getRawY();
+                        break;
+                    case MotionEvent.ACTION_UP: //抬起
+                        //鼠标抬起后记录当前的位置
+                        SharedPreferences.Editor edit = mPref.edit();
+                        edit.putInt("lastX",ivDrag.getLeft());
+                        edit.putInt("lastY",ivDrag.getTop());
+                        edit.apply();
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+}
